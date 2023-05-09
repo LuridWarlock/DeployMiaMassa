876aeb0c2b51df8e82466fe479d3121d6b0b541b
LuridWarlock <133038180+LuridWarlock@users.noreply.github.com>
2023-05-09 13:20:52 -0400

    Update App.jsx

--- cliente/src/App.jsx
+++ cliente/src/App.jsx
@@ -12,8 +12,8 @@ function App() {
   return (
     <Router>
       <Routes>
-        <Route exact path="/" Component={FrontPage} />
-        <Route exact path="/administrador" Component={Administrador} />
+        <Route basename="/" Component={FrontPage} />
+        <Route basename="/administrador" Component={Administrador} />
       </Routes>
     </Router>
   );
