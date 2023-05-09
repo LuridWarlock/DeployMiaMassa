11c7f6093ae71121ee20750bb17e3ac35761b47a
LuridWarlock <133038180+LuridWarlock@users.noreply.github.com>
2023-05-09 14:30:46 -0400

    Update FrontPage.jsx

--- cliente/src/pages/FrontPage.jsx
+++ cliente/src/pages/FrontPage.jsx
@@ -1,16 +1,25 @@
 import React from 'react'
-import "./Administrador.css"
+import "./FrontPage.css"
 
 const FrontPage = () => {
   return (
-    <div className='cuerpoAdministrador'>
-      <div className="crud">
-        Holanda
-      </div>
-      
-
+    <div className='cuerpo'>
+      <NavBar 
+        handleClickPromociones={handleClickPromociones} 
+        handleClickEspecialidades={handleClickEspecialidades} 
+        handleClickAgregados={handleClickAgregados}
+        handleClickExtras={handleClickExtras} 
+        handleClickContacto={handleClickContacto}
+        />
+      <Promociones ref={refPromociones} titulo={"Promociones!"} />
+      <Especialidades ref={refEspecialidades} titulo={"Especialidades"} />
+      <Agregados ref={refAgregados} titulo={"Agregados"} />
+      <Extras ref={refExtras} titulo={"Extras"}/>
+      <Contacto ref={refContacto} titulo={"Contacto"}/>
+      <button id='arriba' onClick={volverArriba}>UP</button>
+      <div className="footer"><span className='footer'></span></div>
     </div>
-  )
+  );
 }
 
 export default FrontPage
