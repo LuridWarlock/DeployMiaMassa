56a76733c3b63c5d1f1dbb46c367e914b377becd
LuridWarlock <133038180+LuridWarlock@users.noreply.github.com>
2023-05-09 14:34:34 -0400

    Update FrontPage.jsx

--- cliente/src/pages/FrontPage.jsx
+++ cliente/src/pages/FrontPage.jsx
@@ -8,6 +8,31 @@ import Extras from '../components/Extras'
 import Contacto from '../components/Contacto'
 
 const FrontPage = () => {
+  const refPromociones = useRef(null);
+  const refEspecialidades = useRef(null);
+  const refContacto = useRef(null);
+  const refAgregados = useRef(null);
+  const refExtras = useRef(null);
+
+  const handleClickPromociones = () => {
+    refPromociones.current?.scrollIntoView({ block: 'start', blockOffset: 10, behavior: 'smooth' });
+  }
+  const handleClickEspecialidades = () => {
+    refEspecialidades.current?.scrollIntoView({ behavior: 'smooth' });
+  }
+  const handleClickAgregados = () => {
+    refAgregados.current?.scrollIntoView({ behavior: 'smooth' });
+  }
+  const handleClickContacto = () => {
+    refContacto.current?.scrollIntoView({ behavior: 'smooth' });
+  }
+  const handleClickExtras = () => {
+    refExtras.current?.scrollIntoView({ behavior: 'smooth' });
+  }
+  const volverArriba = () => {
+    window.scrollTo({ top: 0, behavior: 'smooth' })
+  }
+  
   return (
     <div className='cuerpoAdministrador'>
       <div className="crud">
