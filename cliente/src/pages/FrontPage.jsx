7d59fcc698dd66c8b54d5dd5a0144147b0033d05
LuridWarlock <133038180+LuridWarlock@users.noreply.github.com>
2023-05-09 14:36:03 -0400

    Update FrontPage.jsx

--- cliente/src/pages/FrontPage.jsx
+++ cliente/src/pages/FrontPage.jsx
@@ -33,13 +33,22 @@ const FrontPage = () => {
     window.scrollTo({ top: 0, behavior: 'smooth' })
   }
   
-  return (
-    <div className='cuerpoAdministrador'>
-      <div className="crud">
-        Holanda
-      </div>
-      
-
+ return (
+    <div className='cuerpo'>
+      <NavBar 
+        handleClickPromociones={handleClickPromociones} 
+        handleClickEspecialidades={handleClickEspecialidades} 
+        handleClickAgregados={handleClickAgregados}
+        handleClickExtras={handleClickExtras} 
+        handleClickContacto={handleClickContacto}
+        />
+      <Promociones ref={refPromociones} titulo={"Promociones!"} />
+      <Especialidades ref={refEspecialidades} titulo={"Especialidades"} />
+      <Agregados ref={refAgregados} titulo={"Agregados"} />
+      <Extras ref={refExtras} titulo={"Extras"}/>
+      <Contacto ref={refContacto} titulo={"Contacto"}/>
+      <button id='arriba' onClick={volverArriba}>UP</button>
+      <div className="footer"><span className='footer'></span></div>
     </div>
   )
 }
