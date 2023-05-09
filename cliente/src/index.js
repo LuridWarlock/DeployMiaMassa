8a31ee2ea26754b8ca32c5bc1791b60d13a5ce86
LuridWarlock <133038180+LuridWarlock@users.noreply.github.com>
2023-05-09 14:00:26 -0400

    Update index.js

--- cliente/src/index.js
+++ cliente/src/index.js
@@ -1,11 +1,12 @@
 import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
+import { HashRouter as Router, Route } from 'react-router-dom';
 import "./index.css"
 
 ReactDOM.render(
- <Router>
-    <Route path="/" Component={App} />
- </Router>,
+  <Router>
+    <Route exact path="/" Component={App} />
+  </Router>,
   document.getElementById('root')
 );
