300ce43b6779653cb92942e15350200e350b945e
LuridWarlock <133038180+LuridWarlock@users.noreply.github.com>
2023-05-09 14:03:39 -0400

    Update index.js

--- cliente/src/index.js
+++ cliente/src/index.js
@@ -1,12 +1,11 @@
 import React from 'react';
-import ReactDOM from 'react-dom';
+import ReactDOM from 'react-dom/client';
 import App from './App';
-import { BrowserRouter as Router, Route } from 'react-router-dom';
 import "./index.css"
 
-ReactDOM.render(
-  <Router>
-    <Route exact path="/" Component={App} />
-  </Router>,
-  document.getElementById('root')
+const root = ReactDOM.createRoot(document.getElementById('root'));
+root.render(
+  <React.StrictMode>
+    <App/>
+  </React.StrictMode>
 );
