7cfc8008582d9b20f6980e7324afc221f85a6130
LuridWarlock <133038180+LuridWarlock@users.noreply.github.com>
2023-05-09 14:28:06 -0400

    Update FrontPage.jsx

--- cliente/src/pages/FrontPage.jsx
+++ cliente/src/pages/FrontPage.jsx
@@ -1,7 +1,7 @@
 import React from 'react'
 import "./Administrador.css"
 
-const Administrador = () => {
+const FrontPage = () => {
   return (
     <div className='cuerpoAdministrador'>
       <div className="crud">
