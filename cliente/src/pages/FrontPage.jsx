183c318bdc3abfe6a38a1354537d22bb3d92088d
LuridWarlock <133038180+LuridWarlock@users.noreply.github.com>
2023-05-09 14:32:54 -0400

    Update FrontPage.jsx

--- cliente/src/pages/FrontPage.jsx
+++ cliente/src/pages/FrontPage.jsx
@@ -9,23 +9,14 @@ import Contacto from '../components/Contacto'
 
 const FrontPage = () => {
   return (
-    <div className='cuerpo'>
-      <NavBar 
-        handleClickPromociones={handleClickPromociones} 
-        handleClickEspecialidades={handleClickEspecialidades} 
-        handleClickAgregados={handleClickAgregados}
-        handleClickExtras={handleClickExtras} 
-        handleClickContacto={handleClickContacto}
-        />
-      <Promociones ref={refPromociones} titulo={"Promociones!"} />
-      <Especialidades ref={refEspecialidades} titulo={"Especialidades"} />
-      <Agregados ref={refAgregados} titulo={"Agregados"} />
-      <Extras ref={refExtras} titulo={"Extras"}/>
-      <Contacto ref={refContacto} titulo={"Contacto"}/>
-      <button id='arriba' onClick={volverArriba}>UP</button>
-      <div className="footer"><span className='footer'></span></div>
+    <div className='cuerpoAdministrador'>
+      <div className="crud">
+        Holanda
+      </div>
+      
+
     </div>
-  );
+  )
 }
 
 export default FrontPage
