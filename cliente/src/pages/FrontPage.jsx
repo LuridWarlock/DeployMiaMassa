2ad1366e76c13f5e7d13b4d0288e9c3d67d2b347
LuridWarlock <133038180+LuridWarlock@users.noreply.github.com>
2023-05-09 14:31:52 -0400

    Update FrontPage.jsx

--- cliente/src/pages/FrontPage.jsx
+++ cliente/src/pages/FrontPage.jsx
@@ -1,5 +1,11 @@
-import React from 'react'
-import "./FrontPage.css"
+import { useRef, useEffect, useState } from 'react'
+import './FrontPage.css'
+import Especialidades from "../components/Especialidades"
+import NavBar from '../components/NavBar'
+import Promociones from '../components/Promociones'
+import Agregados from '../components/Agregados'
+import Extras from '../components/Extras'
+import Contacto from '../components/Contacto'
 
 const FrontPage = () => {
   return (
